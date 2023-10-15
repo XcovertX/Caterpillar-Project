4bf63d4adcb4c809a78b38af96713a08210c481d
James Covert <jcovert2@asu.edu>
2023-10-15 13:26:50 -0700

    added modal functionality

---
+++ cat-app/app/components/button.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+type Props = {
+    label: boolean,
+    secondary: boolean,
+    fullWidth: boolean,
+    onClick: any,
+    disabled: boolean,
+    outline: boolean,
+    large: boolean
+}
+
+const Button = ({
+  label,
+  secondary,
+  fullWidth,
+  onClick,
+  disabled,
+  outline,
+  large,
+}: Props) => {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={`disabled:opacity-50 disabled:cursor-not-allowed rounded-full font-semibold hover:opacity-80 transition border-2 
+    ${fullWidth ? "w-full" : "w-fit"}
+    ${secondary ? "bg-white" : "bg-sky-500"}
+    ${secondary ? "text-black" : "text-white"}
+    ${secondary ? "border-black" : "border-sky-500"}
+    ${large ? "text-xl" : "text-md"}
+    ${large ? "px-5" : "px-4"}
+    ${large ? "py-3" : "py-2"}
+    ${outline ? "bg-transparent" : ""}
+    ${outline ? "border-white" : ""}
+    ${outline ? "text-white" : ""}
+    `}
+    >
+      {label}
+    </button>
+  );
+}
+
+export default Button;
